6b85d8b4f45e86e98262cdda6e7a580569dcb7d2
Amol Morey <amolmorey24@gmail.com>
2016-04-27 14:42:07 +0530

    new function added

--- cal.py
+++ cal.py
@@ -1,3 +1,6 @@
 #function defination
 def square(x):
 	return x**2
+
+def cube(x):
+	return x*x*x
--- test_maths.py
+++ test_maths.py
@@ -4,3 +4,7 @@ import cal
 def test_square():
     x = 4
     assert cal.square(x) == 16
+
+def test_cube():
+    x = 4
+    assert cal.cube(x) == 64
